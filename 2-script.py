0470e78c6379ab2cc8a7ef97d859608332ba1bc8
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2024-08-07 11:52:04 -0600

    upload init py files

---
+++ 2-script.py
@@ -0,0 +1,13 @@
+#import numpy as np
+import pygame
+
+'''
+Tarea: Investigar qué es el Cálculo Diferencial (qué es la derivada)
+'''
+
+
+pygame.init()
+screen = pygame.display.set_mode((250, 250))
+clock = pygame.time.Clock()
+running = True
+dt = 0
---
+++ init.py
@@ -0,0 +1,7 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import cv2
+
+for i in range (10):
+    print ("El valor de i es: ", i)
+    print ("Hello from FCC")
